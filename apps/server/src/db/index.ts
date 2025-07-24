ece04c1e0eab05e903d7dd43b6b531e55ebd4ed7
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2025-07-24 19:38:33 +0700

    refactor: change api from bundler to node

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
 
 const supabaseUrl = process.env.DATABASE_URL || "";
 const supabaseKey = process.env.API_KEY || "";
